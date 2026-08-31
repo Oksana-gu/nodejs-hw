dceeb40384ed35261e3519af7223c7dbafdd9519
Oksana-gu <guraoxana@gmail.com>
2026-08-31 22:12:13 +0300

    03-validation

---
+++ src/models/note.js
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    content: {
+      type: String,
+      default: '',
+    },
+
+    tag: {
+      type: String,
+      enum: TAGS,
+      default: 'Todo',
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', noteSchema);
